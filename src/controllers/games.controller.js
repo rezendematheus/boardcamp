758761fbb0066f7172ce8cb28ee32007ade4b363
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-02-10 12:03:41 -0300

    feat: controller post /games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -15,10 +15,15 @@ export async function getGames(req, res){
 
 export async function insertGame(req, res){
     try {
+        console.log(req.body.name)
         const {name, image, stockTotal, pricePerDay} = req.body;
         
-        const nameExist = await d
+        const nameExist = await db.query(`SELECT * FROM games WHERE name='${name}'`)
+        if(nameExist.rows[0]) res.status(409).send()
+        
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ('${name}', '${image}', ${stockTotal}, ${pricePerDay})`)
 
+        res.status(201).send()
     } catch (error) {
         console.log(error)
     }
